3b6d395f6a30c004ecca6794b8dfe71ca538fbc1
Tishkin <Dmitriy.Tishkin@infowatch.com>
2017-09-25 16:51:21 +0300

    = localizer: semi-finalizing the program;

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 #!/usr/bin/env node
 "use strict";
 
-var fs = require("fs");
-var _ = require("lodash");
-var args = process.argv.slice(2);
-const { exec } = require('child_process');
+var fs       = require("fs");
+var _        = require("lodash");
+var { exec } = require('child_process');
+var path     = require('path');
 
-/** Input variables
- * arguments:
- * first  - directory where will find keys
- * second - path where will create or change json files
- * third  - type files for search in directory
- * fourth - regex for search keys
- */
-var PATH_INPUT  = args[0]; 
-var PATH_OUTPUT = args[1];
-var LANGUAGES   = ["ru", "en"]
-var TYPES_JSON  = new RegExp("([aA-zZ\-]*)\/i18n\/(" + LANGUAGES.join("|") + ")\.json$");
-var KEY_REGEX   = /{{ '([aA-zZ0-9._]*)' \| translate }}/g;
+// User variables
+var args       = process.argv.slice(2);
+
+var isGit      = commander("--git");
+var isConfig   = commander("--config");
+var PATH_INPUT = args[0];
+
+var { PATH_OUTPUT, LANGUAGES, KEY_REGEX, ALLOW_CREATE } = getConfig();
 
 // Initialize variebles
-var DIFF_KEYS   = {};
-var FILES      = [];
-
-var OLD_KEYS   = {};
+    KEY_REGEX  = new RegExp(KEY_REGEX, 'g');
+var TYPES_JSON = new RegExp("([aA-zZ\-]*)\/i18n\/(" + LANGUAGES.join("|") + ")\.json$");
+var FILES      = getFiles(PATH_INPUT);
 var JSON_FILES = [];
-
-var NEW_KEYS = {};
+var DIFF_KEYS  = {};
+var OLD_KEYS   = {};
+var NEW_KEYS   = {};
 
 LANGUAGES.forEach((lang) => {
-  DIFF_KEYS[lang]  = {};
+  DIFF_KEYS[lang] = {};
   OLD_KEYS[lang]  = {};
 });
 
@@ -39,6 +35,37 @@ start();
 function start() {
   console.log('[INFO] Starting directory: ' + process.cwd() + '\n');
 
+  if(isGit)
+    localizerWithGitChange();
+  else
+    localizer();
+}
+
+function localizer(){
+  // Get new keys
+  console.log('[INFO] Found follow new keys of localization:');
+  readFiles(FILES, (data, file) => {
+    var key
+    console.log(KEY_REGEX)
+    while((key = KEY_REGEX.exec(data)) != null){
+      setByKey(DIFF_KEYS, key[1], '! NEW !');
+      console.log(" + " + key[1]);
+    }
+  });   
+
+  // Get diff keys
+  JSON_FILES = getFilesByTypes(TYPES_JSON);
+  readFiles(JSON_FILES, (data, file) => {
+    let charts = file.match(TYPES_JSON)
+    OLD_KEYS[charts[2]][charts[1]] = JSON.parse(data);
+  });
+  console.log('\n[INFO] Detected follow files of localization:\n'+ JSON_FILES.join("\n") + "\n");
+
+  mergeKeys();
+  editOrCreateFiles();
+}
+
+function localizerWithGitChange(){
   exec('git status -s | cut -c4- | grep "' + PATH_INPUT + '"', (error, stdout) => {
     if (error) {
       console.error('exec error: ${error}');
@@ -50,27 +77,8 @@ function start() {
     }
     console.log('[INFO] Found change files by "'+ PATH_INPUT +'":\n' + stdout);
 
-    // Get new keys
     FILES = stdout.split('\n');FILES.pop();
-    console.log('[INFO] Found follow new keys of localization:');
-    readFiles(FILES, (data) => {
-      var key
-      while((key = KEY_REGEX.exec(data)) != null){
-        setByKey(DIFF_KEYS, key[1], '! NEW !');
-        console.log(key[1]);
-      }
-    });   
-
-    // Get old keys
-    JSON_FILES = getFilesByTypes(TYPES_JSON);
-    readFiles(JSON_FILES, (data, file) => {
-      let charts = file.match(TYPES_JSON)
-      OLD_KEYS[charts[2]][charts[1]] = JSON.parse(data);
-    });
-    console.log('\n[INFO] Detected follow files of localization:\n'+ JSON_FILES.join("\n") + "\n");
-
-    mergeKeys();
-    editOrCreateFiles();
+    localizer();
   });
 }
 
@@ -79,11 +87,15 @@ function editOrCreateFiles(){
   LANGUAGES.forEach((lang) => {
     let plugins = _.keys(NEW_KEYS[lang]);
     plugins.forEach((plugin) => {
-      let fileName = PATH_OUTPUT + plugin +  '/i18n/' + lang + '.json';
+      let path = './' + PATH_OUTPUT + plugin +  '/i18n/' + lang;
+      let fileName =  path + '.json';
       if(_.isEqual(NEW_KEYS[lang][plugin], OLD_KEYS[lang][plugin]))
         return;
 
-      fs.writeFile(fileName, JSON.stringify(NEW_KEYS[lang][plugin], null, 2) + '\n', 'utf8', function (err) {
+      if(ALLOW_CREATE)
+        ensureDirectoryExistence(path);
+
+      fs.writeFile(fileName, JSON.stringify(NEW_KEYS[lang][plugin], null, 2) + '\n', {flag: "wx+"}, function (err) {
         if (err) {
             return console.log(err);
         }
@@ -91,12 +103,6 @@ function editOrCreateFiles(){
         console.log(' = ' + fileName);
       });
     });
-  });
-}
-
-function setByKey(object, element, value) {
-  LANGUAGES.forEach((lang) => {
-    _.set(object[lang], element, value);
   });
 }
 
@@ -110,6 +116,9 @@ function mergeKeys() {
 function readFiles(files, callback) {
   files.forEach((file) => {
     try{
+      if(!fs.existsSync(file))
+        return;
+
       let data = fs.readFileSync(file, 'utf8');
       callback(data, file);
     }
@@ -121,7 +130,7 @@ function readFiles(files, callback) {
 
 // Methods for search files by types
 function getFilesByTypes(types) {
-  let files = _getFiles(PATH_INPUT);
+  let files = getFiles(PATH_INPUT);
 
   files = files.filter((file) => {
     return types.test(file);
@@ -130,7 +139,7 @@ function getFilesByTypes(types) {
   return files;
 }
 
-function _getFiles(path) {
+function getFiles(path) {
   let results = [];
   let list = fs.readdirSync(path);
 
@@ -139,10 +148,67 @@ function _getFiles(path) {
     let stat = fs.statSync(file);
 
     if (stat && stat.isDirectory())
-      results = results.concat(_getFiles(file));
+      results = results.concat(getFiles(file));
     else
       results.push(file);
   });
 
   return results;
+}
+
+
+//helpers
+
+function setByKey(object, element, value) {
+  LANGUAGES.forEach((lang) => {
+    _.set(object[lang], element, value);
+  });
+}
+
+function getConfig(){
+  let conf = {
+    PATH_OUTPUT : "src/plugin/",
+    LANGUAGES   : ["ru", "en"],
+    KEY_REGEX   : "/{{ '([aA-zZ0-9._]*)' \\| translate }}",
+    ALLOW_CREATE : true,
+    ALLOW_REMOVE : false
+  }
+
+  if(isConfig){
+    try {
+      conf = require(process.cwd() + '/ng-localizer.config.json');
+      console.log("[INFO] config file detected");
+      return conf;
+    } 
+    catch (ex) {
+      fs.writeFile('./ng-localizer.config.json', JSON.stringify(conf, null, 2) + '\n', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+      });
+
+      return conf;
+    }
+  }
+
+  return conf;
+}
+
+function commander(command){
+  let index = args.indexOf(command);
+  if(index != -1) {
+    console.log(index);
+    args.splice(index, 1);
+    return true
+  }
+  return false;
+}
+
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
 }
